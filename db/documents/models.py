c2ec410d8a61ae6f99502b68efd5127ad528c691
XXmmRR <Xorex50@protonmail.com>
2024-02-14 23:41:00 +0500

    add odm and init collections

---
+++ db/documents/models.py
@@ -0,0 +1,49 @@
+from beanie import Document, Indexed, init_beanie, Link
+from typing import List
+
+
+class Address(Document):
+    lat: float
+    lon: float
+    name: str
+    orient: str
+
+
+class User(Document):
+    first_name: str
+    last_name: str
+    phone: str
+    password: str
+    user_type: str
+    favorites: List[Link['Institution']]
+    address: List[Link[Address]]
+
+
+
+class Tag(Document):
+    tag_name: str
+    draft: bool
+
+
+class Rating(Document):
+    starts: float
+    user: Link[User]
+    institution: Link['Institution']    
+
+
+class Institution(Document):
+    name: Indexed(str)
+    image: str
+    description: str
+    owner: Link[User]
+    tags: List[Link[Tag]]
+    
+
+class Food(Document):
+    name: str
+    description: str
+    image: str
+    price: int
+    draft: bool
+    institution: Link[Institution]
+    
--- db/mongodb_utils.py
+++ db/mongodb_utils.py
@@ -2,20 +2,27 @@ import logging
 from beanie import init_beanie, Document
 from pydantic import BaseModel
 from typing import Optional
-
 from motor.motor_asyncio import AsyncIOMotorClient
 from core.config import MONGODB_URL, MAX_CONNECTIONS_COUNT, MIN_CONNECTIONS_COUNT
 from .mongodb import db
 from beanie import init_beanie
-    
+from db.documents.models import Address, Institution, User, Food, Tag, Rating
+
+
 
 async def connect_to_mongo():
     logging.info("mongo has started")
     db.client = AsyncIOMotorClient(str(MONGODB_URL),
                                    maxPoolSize=MAX_CONNECTIONS_COUNT,
                                    minPoolSize=MIN_CONNECTIONS_COUNT)
-    print(db)
-    await init_beanie(database=db.client.food_shift, document_models=[])
+    await init_beanie(database=db.client.food_shift, document_models=[
+                                                                Address,
+                                                                User,
+                                                                Food,
+                                                                Tag,
+                                                                Rating,
+                                                                Institution
+        ])
 
 
 async def close_mongo_connection():
