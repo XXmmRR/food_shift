dd6bd7cabd34cdb3a26375e65c7fc5574607440b
XXmmRR <Xorex50@protonmail.com>
2024-02-15 00:39:04 +0500

    add jwt auth

---
+++ core/jwt.py
@@ -0,0 +1,32 @@
+
+from fastapi_jwt import JwtAuthorizationCredentials, JwtAccessBearer, JwtRefreshBearer
+from datetime import timedelta
+from db.models import User
+from core.config import SECRET_KEY
+
+
+ACCESS_EXPIRES = timedelta(minutes=15)
+REFRESH_EXPIRES = timedelta(days=30)
+
+access_security = JwtAccessBearer(
+    SECRET_KEY,
+    access_expires_delta=ACCESS_EXPIRES,
+    refresh_expires_delta=REFRESH_EXPIRES,
+)
+
+refresh_security = JwtRefreshBearer(
+    SECRET_KEY,
+    access_expires_delta=ACCESS_EXPIRES,
+    refresh_expires_delta=REFRESH_EXPIRES,
+)
+
+
+async def user_from_credentials(auth: JwtAuthorizationCredentials) -> User | None:
+    """Return the user associated with auth credentials."""
+    return await User.by_email(auth.subject["username"])
+
+
+async def user_from_token(token: str) -> User | None:
+    """Return the user associated with a token value."""
+    payload = access_security._decode(token)
+    return await User.by_email(payload["subject"]["username"])
