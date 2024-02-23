1d948017de31dab73ea1487eb1e17443f35cb4cc
XXmmRR <Xorex50@protonmail.com>
2024-02-23 21:22:10 +0500

    add dependesies for user and institution

---
+++ api/depends/institution/current_institution.py
@@ -0,0 +1,15 @@
+"""Current institution dependency."""
+
+from fastapi import HTTPException
+
+from db.models import Institution
+
+async def current_institution(
+    institution_name: str
+    ) -> Institution:
+    """Return the current institution"""
+    institution = await Institution.find_one(Institution.InstitutionName==institution_name)
+    if institution is None:
+        raise HTTPException(404, "Institution not found")
+    return institution
+
