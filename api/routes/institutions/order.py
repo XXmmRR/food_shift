a162d54f0cfeabf5e184411501363a16aaaacac0
XXmmRR <Xorex50@protonmail.com>
2024-02-27 18:41:46 +0500

    make order creation

--- api/routes/institutions/order.py
+++ api/routes/institutions/order.py
@@ -1,28 +1,41 @@
 """Order router."""
 
-from fastapi import APIRouter, HTTPException, UploadFile, Depends
+from fastapi import APIRouter, Depends, Security
 from fastapi import APIRouter, HTTPException
-from db.models import Order, OrderItem
+from db.models import Order, OrderItem, Food
 from typing import List
 from pymongo.errors import DuplicateKeyError
 from schemas.institutuions.order import (
     OrderModelItem,
     OrderModel,
 )
-
+from core.jwt import access_security
+from fastapi_jwt import JwtAuthorizationCredentials
+from api.depends.user.current_user import current_user
 from utils.pydantic_encoder import encode_input
+from schemas.institutuions.order import OrderModel
 from api.depends.institution.current_institution import current_institution
-
+from db.models import User
 
 
 router = APIRouter(prefix="/order", tags=["Order"])
 
-@router.post('/{institution_name}')
+@router.post('/{institution_name}', response_model=OrderModel)
 async def create_order(
                        order: OrderModel,
                        institution=Depends(current_institution),
+                       auth: JwtAuthorizationCredentials = Security(access_security),
+                       user: User = Depends(current_user)
                        ):
+    orders = []
+    price = 0
     for i in order.orders_items:
-        order_obj = OrderItem(**i)
+        food = await Food.find_one(Food.name == i.food.name)
+        price += food.price
+        order_obj = OrderItem(food=food, quantity=i.quantity, user=user)
+        orders.append(order_obj)
         await order_obj.save()
-        
+    order = Order(order_items=orders, user=user, institution=institution, price=price)
+    return order
+
+
